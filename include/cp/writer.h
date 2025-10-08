ba48b7e04c1a11b7cf152b029b59780a2466cfcf
Gabriel S. Colombo <gscolombo404@gmail.com>
2025-10-08 10:33:59 -0300

    Deleção de estrutura obselata

--- include/cp/writer.h
+++ include/cp/writer.h
@@ -12,34 +12,6 @@
 #include "wchar.h"
 
 /**
- * @struct RefInfo
- * @brief Representa informações de referência para uma classe, incluindo o nome da classe, nome, tipo e índices associados.
- *
- * Esta estrutura armazena metadados sobre uma referência de classe, incluindo o nome, tipo e índices
- * associados em um pool de constantes.
- *
- * @var RefInfo::cls
- * Ponteiro para uma string de caracteres largos representando o nome da classe.
- *
- * @var RefInfo::ref_name
- * Ponteiro para uma string de caracteres largos representando o identificador da referência.
- *
- * @var RefInfo::ref_type
- * Ponteiro para uma string de caracteres largos representando a descrição do tipo da referência.
- *
- * @var RefInfo::cls_index
- * Inteiro sem sinal de 16 bits representando o índice da classe em um pool de constantes.
- *
- * @var RefInfo::name_and_type_index
- * Inteiro sem sinal de 16 bits representando o índice do nome e tipo em um pool de constantes.
- */
-struct RefInfo
-{
-    wchar_t *cls, *ref_name, *ref_type;
-    u2 cls_index, name_and_type_index;
-};
-
-/**
  * @brief Exibe as constantes do pool de constantes.
  *
  * Esta função recupera e exibe as constante do pool de constantes.
