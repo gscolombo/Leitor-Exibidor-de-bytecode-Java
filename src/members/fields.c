b3797ccfc8de0746b2c32666d62bc761e679e106
Gabriel S. Colombo <gscolombo404@gmail.com>
2025-10-12 23:37:03 -0300

    Inclusão de modificadores de campos de classe durante exibição

--- src/members/fields.c
+++ src/members/fields.c
@@ -11,6 +11,15 @@ static const FlagMap flag_map[9] = {
     {0x1000, "ACC_SYNTHETIC"},
     {0x4000, "ACC_ENUM"}};
 
+static const FlagMap flag_kw_map[7] = {
+    {0x0001, "public"},
+    {0x0002, "private"},
+    {0x0004, "protected"},
+    {0x0008, "static"},
+    {0x0010, "final"},
+    {0x0040, "volatile"},
+    {0x0080, "transient"}};
+
 void show_fields(const ClassFile *cf)
 {
     if (cf->constant_pool == NULL)
@@ -34,11 +43,44 @@ void show_fields(const ClassFile *cf)
 
             char *flags = get_access_flags(access_flags, 9, flag_map);
 
-            printf(" %ls%ls;\n    descriptor: %ls\n    flags: (0x%04x) %s\n%c",
-                   field_desc_str, field_name, field_desc,
+            size_t buffer_size = 256;
+            char *kws = malloc(buffer_size);
+            if (kws != NULL)
+            {
+                char *ptr = kws;
+
+                for (size_t i = 0; i < 7; i++)
+                {
+                    if (access_flags & flag_kw_map[i].flag)
+                    {
+                        size_t l = strlen(flag_kw_map[i].name);
+
+                        if (buffer_size > l + 2)
+                        {
+                            strcpy(ptr, flag_kw_map[i].name);
+                            ptr += l;
+                            buffer_size -= l;
+                            *ptr++ = ' ';
+                            buffer_size--;
+                        }
+                    }
+                }
+
+                if (ptr > kws)
+                    ptr--; // Remove trailing space
+                *ptr = '\0';
+
+                char *tmp = realloc(kws, buffer_size);
+                if (tmp)
+                    kws = tmp;
+            }
+
+            printf(" %s %ls%ls;\n    descriptor: %ls\n    flags: (0x%04x) %s\n%c",
+                   kws, field_desc_str, field_name, field_desc,
                    access_flags, flags, nl);
 
             free(flags);
             free(field_desc_str);
+            free(kws);
         }
 }
