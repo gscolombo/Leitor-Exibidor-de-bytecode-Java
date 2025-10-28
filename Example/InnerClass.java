4ff508e3ee2a39a44bbd1ed63cf13f9f9be52e14
Gabriel S. Colombo <gscolombo404@gmail.com>
2025-10-28 11:46:18 -0300

    Inclusão de segunda classe aninhada em exemplo

--- Example/InnerClass.java
+++ Example/InnerClass.java
@@ -3,16 +3,29 @@ class Outer {
     private String outerMessage = "Hello from Outer class";
     
     // Inner class
-    class Inner {
+    class Foo {
         public void display() {
-            System.out.println("Inside Inner class");
+            System.out.println("Inside Foo class");
+            System.out.println("Accessing: " + outerMessage);
+        }
+    }
+
+    class Bar {
+        public void display() {
+            System.out.println("Inside Bar class");
             System.out.println("Accessing: " + outerMessage);
         }
     }
     
-    // Method to create and use inner class
-    public void useInner() {
-        Inner inner = new Inner();
-        inner.display();
+    
+    // Methods to create and use inner classes
+    public void useFoo() {
+        Foo foo = new Foo();
+        foo.display();
+    }
+
+    public void useBar() {
+        Bar bar = new Bar();
+        bar.display();
     }
 }
