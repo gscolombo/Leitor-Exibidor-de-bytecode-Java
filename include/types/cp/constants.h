3bda0a31d62d4417718ac23380c829b70bb53015
Gustavo Rodrigues da Costa <31167914+gusrodrigues@users.noreply.github.com>
2025-10-28 07:36:57 -0300

    Reorganize constant definitions in constants.h

--- include/types/cp/constants.h
+++ include/types/cp/constants.h
@@ -2,21 +2,20 @@
 #define TYPES_CP_CONSTANTS_H
 
 /* Tags de constantes */
-#define CONSTANT_Class 7
-#define CONSTANT_Fieldref 9
-#define CONSTANT_Methodref 10
-#define CONSTANT_InterfaceMethodref 11
-#define CONSTANT_String 8
+#define CONSTANT_UTF8 1
 #define CONSTANT_Integer 3
 #define CONSTANT_Float 4
 #define CONSTANT_Long 5
 #define CONSTANT_Double 6
+#define CONSTANT_Class 7
+#define CONSTANT_String 8
+#define CONSTANT_Fieldref 9
+#define CONSTANT_Methodref 10
+#define CONSTANT_InterfaceMethodref 11
 #define CONSTANT_NameAndType 12
-#define CONSTANT_UTF8 1
 #define CONSTANT_MethodHandle 15
 #define CONSTANT_MethodType 16
 #define CONSTANT_InvokeDynamic 18
-
 #include "uinteger.h"
 
 /** @file */
