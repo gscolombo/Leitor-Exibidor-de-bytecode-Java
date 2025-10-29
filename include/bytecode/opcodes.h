aff194c0f535477fae3e0b3458d11057369ed30d
Gabriel S. Colombo <gscolombo404@gmail.com>
2025-10-28 21:00:15 -0300

    Uso de definição de tipos em uinteger.h

--- include/bytecode/opcodes.h
+++ include/bytecode/opcodes.h
@@ -5,18 +5,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef uint8_t u1;
-typedef uint32_t u4;
-typedef uint16_t u2;
+#include "uinteger.h"
 
-
-typedef struct {
+typedef struct
+{
     u1 opcode;
-    const char* name;
+    const char *name;
     int category;
 } OpcodeInfo;
 
-const OpcodeInfo* get_opcode_info(u1 opcode);
+const OpcodeInfo *get_opcode_info(u1 opcode);
 
 void show_opcodes(const u1 *code, u4 length);
 
